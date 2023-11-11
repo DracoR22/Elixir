6b9290ab6f5db157fa40cec84c18a16a64c21495
DracoR22 <rdraco039@gmail.com>
2023-11-10 23:10:42 -0500

    archived

--- components/main/Navigation.tsx
+++ components/main/Navigation.tsx
@@ -1,21 +1,21 @@
 'use client'
 
 import { cn } from "@/lib/utils"
-import { ChevronsLeft, MenuIcon, PlusCircle } from "lucide-react" 
+import { ChevronsLeft, MenuIcon, PlusCircle, Search, Settings } from "lucide-react" 
 import { usePathname } from "next/navigation"
 import { ElementRef, useRef, useState, useEffect } from "react"
 import { useMediaQuery } from "usehooks-ts"
 import UserItem from "./UserItem"
-import { useQuery, useMutation } from "convex/react"
+import { useMutation } from "convex/react"
 import { api } from "@/convex/_generated/api"
 import Item from "./Item"
 import { toast } from "sonner"
+import DocumentList from "./DocumentList"
 
 const Navigation = () => {
 
    const pathname = usePathname()
    const isMobile = useMediaQuery("(max-width: 768px)")
-   const documents = useQuery(api.documents.get)
    const create = useMutation(api.documents.create)
 
    const isResizingRef = useRef(false)
@@ -110,14 +110,12 @@ const Navigation = () => {
         </div>
       <div>
         <UserItem/>
+        <Item label="Search" icon={Search} isSearch onClick={() => {}}/>
+        <Item label="Settings" icon={Settings} onClick={() => {}}/>
         <Item onClick={handleCreate} label="New page" icon={PlusCircle}/>
       </div>
       <div className="mt-4">
-        {documents?.map((document) =>(
-          <p key={document._id}>
-            {document.title}
-          </p>
-        ))}
+        <DocumentList/>
       </div>
       {/* THIS IS THE CHANGE SIDEBAR WIDTH LINE */}
       <div onMouseDown={handleMouseDown} onClick={resetWidth} className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"/>
